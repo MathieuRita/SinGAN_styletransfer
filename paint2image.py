f46e3fb550c56a47701699a41896219f509e02f7
Mathieu <mathieu.rita@polytechnique.edu>
2020-01-12 16:26:20 +0100

    changes

--- paint2image.py
+++ paint2image.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
             #in_s = in_s[:, :, :reals[n].shape[2], :reals[n].shape[3]]
             opt.gen_start_scale=0
             in_s = torch.full(reals[0].shape, 0, device=opt.device)
-            plt.imsave('input_random' % (dir2save, opt.paint_start_scale), functions.convert_image_np(in_s.detach()), vmin=0, vmax=1)
+            plt.imsave('input_random', functions.convert_image_np(in_s.detach()), vmin=0, vmax=1)
 
             if opt.quantization_flag:
                 opt.mode = 'paint_train'
